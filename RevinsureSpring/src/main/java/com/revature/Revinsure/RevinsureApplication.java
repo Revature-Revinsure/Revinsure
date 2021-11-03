f23f6dc8266ed15a92952193391a7039ad635336
Tyler Nickl <tyler.a.nickl@gmail.com>
2021-11-03 05:03:01 -0700

    initial project setup

---
+++ RevinsureSpring/src/main/java/com/revature/Revinsure/RevinsureApplication.java
@@ -0,0 +1,13 @@
+package com.revature.Revinsure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevinsureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevinsureApplication.class, args);
+	}
+
+}
---
+++ RevinsureSpring/src/test/java/com/revature/Revinsure/RevinsureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.Revinsure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevinsureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
