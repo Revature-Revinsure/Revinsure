f0146c3ee76b2084a05e556eea061124f1947d41
Alexandre Thorp <Thorpalexandre614@gmail.com>
2021-11-05 11:43:34 -0400

    Finished User Controller Update Methods

--- RevinsureSpring/src/main/java/com/revature/Revinsure/controllers/UserController.java
+++ RevinsureSpring/src/main/java/com/revature/Revinsure/controllers/UserController.java
@@ -1,12 +1,100 @@
 package com.revature.Revinsure.controllers;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class UserController {
+import com.revature.Revinsure.models.User;
+import com.revature.Revinsure.models.UserInfo;
+import com.revature.Revinsure.services.UserService;
 
-	public UserController() {
-		// TODO Auto-generated constructor stub
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200/", allowCredentials = "true")
+public class UserController {
+	
+	@Autowired 
+	private UserService uService;
+	
+	public User getUserSession(HttpSession session) {
+		
+		User u = null;
+
+		if(session.getAttribute("user") == null) {
+			
+			u = new User();
+			u.setId(-1);
+			
+		} else {
+			
+			u = (User) session.getAttribute("user");
+			
+		}
+		
+		return u;
+		
+	}
+
+	@PostMapping(value = "/updatePassword")
+	public void updatePassword(@RequestBody String password, HttpServletResponse response, HttpSession session) {
+		
+		User u = this.getUserSession(session);
+		
+		if(u.getId() == -1) {
+			
+			response.setStatus(400);
+			
+		}else {
+			
+			uService.updatePassword(u, password);
+			response.setStatus(200);
+			
+		}
+
+
+	}
+	
+	@PostMapping(value = "/updateEmail")
+	public void updateEmail(@RequestBody String email, HttpServletResponse response, HttpSession session) {
+		
+		User u = this.getUserSession(session);
+		
+		if(u.getId() == -1) {
+			
+			response.setStatus(400);
+			
+		}else {
+			
+			uService.updateEmail(u, email);
+			response.setStatus(200);
+			
+		}
+
+
+	}
+	
+	@PostMapping(value = "/updateUserInfo")
+	public void updateUserInfo(@RequestBody UserInfo userInfo, HttpServletResponse response, HttpSession session) {
+		
+		User u = this.getUserSession(session);
+		
+		if(u.getId() == -1) {
+			
+			response.setStatus(400);
+			
+		}else {
+			
+			uService.updateUserInfo(u, userInfo);
+			response.setStatus(200);
+			
+		}
+
+
 	}
 
 	
