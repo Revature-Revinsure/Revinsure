5ae5161e23769a0384b355065546f088597f6cc0
Tyler Nickl <tyler.a.nickl@gmail.com>
2021-11-05 12:50:50 -0700

    RegisterComponent and Service complete but untested

--- RevinsureAngular/src/app/service/register.service.ts
+++ RevinsureAngular/src/app/service/register.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
+import { UserInfo } from '../models/user-info';
 import { DataService } from './data.service';
 
 @Injectable({
@@ -12,26 +13,35 @@ export class RegisterService {
 
   httpOptions = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-      
-      withCredentials: true, //only one needed in a GET; others are for POST, PUT, ETC
+      withCredentials: true,
       observe: 'response' as 'response'
       }; 
 
   constructor(private httpClient: HttpClient, private dataService: DataService ) { }
   
-  userExists(email: String): Observable<HttpResponse<User>> {
-      let addOnURL: String = '/register';
-      let fullURL: String = this.dataService.BaseURL + addOnURL;
-      return this.httpClient.post<User>(fullURL, {"email": email}, this.httpOptions);
+  userExists(email: string): Observable<HttpResponse<boolean>> {
+    let options = {
+      withCredentials: true,
+      observe: 'response' as 'response',
+      body: {
+        "email": email
       }
+    };
+      let addOnURL: string = '/register';
+      let fullURL: string = this.dataService.BaseURL + addOnURL;
+      return this.httpClient.get<boolean>(fullURL, options);
+  }
   
 
-  registerNewUser(unregisteredUser: User) {
-    this.httpClient.post<>(this.dataService.urlForRegister, { })
+  registerNewUser(unregisteredUser: User): Observable<HttpResponse<User>> {
+    let addOnURL: string = '/register';
+    let fullURL: string = this.dataService.BaseURL + addOnURL;
+    return this.httpClient.post<User>(fullURL, {unregisteredUser}, this.httpOptions)
   }
 
-  registerNewUserInfo(infoForm: {firstname: String; lastname: String; address: String; city: String; state: String; zip: String})
-  
-
-
+  registerNewUserInfo(infoForm: UserInfo): Observable<HttpResponse<UserInfo>> {
+    let addOnURL: string = '/registerInfo';
+    let fullURL: string = this.dataService.BaseURL + addOnURL;
+    return this.httpClient.post<UserInfo>(fullURL, {infoForm}, this.httpOptions)
+  }
 }
