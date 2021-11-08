b5989d16833e2dc2e77c3e5d13199e7df56c3cf0
lilmissrayna <lilmissrayna@gmail.com>
2021-11-08 12:31:59 -0600

    fixed test

--- RevinsureSpring/src/test/java/com/revature/Revinsure/service/UserServiceTest.java
+++ RevinsureSpring/src/test/java/com/revature/Revinsure/service/UserServiceTest.java
@@ -1,7 +1,6 @@
 package com.revature.Revinsure.service;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
