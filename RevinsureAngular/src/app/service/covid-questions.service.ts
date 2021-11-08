189c0f75f70820678f78d93c9d705f9cd8fe6be1
Lauren Smith <ldsmith16@liberty.edu>
2021-11-08 11:52:20 -0600

    covid question models and service

--- RevinsureAngular/src/app/service/covid-questions.service.ts
+++ RevinsureAngular/src/app/service/covid-questions.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { Message } from '../models/message';
+import { CovidQuestion } from '../models/covid-question';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CovidQuestionsService {
 
-  constructor() { }
+  baseUrl: String = "http://localhost:8000";
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+   
+  withCredentials: true, 
+  observe: 'response' as 'response'
+  };  
+
+  constructor(private http: HttpClient) { }
+  
+  //call this method when a user logs in
+  getCurrentForm(): Observable<HttpResponse<Date>>{
+    let addonUrl: string = "";
+    let fullUrl: string = this.baseUrl + addonUrl;
+
+    //I just want the date of the last form
+    return this.http.get<Date>(fullUrl, this.httpOptions);
+  }
+
+  submitNewForm(form: CovidQuestion): Observable<HttpResponse<Message>>{
+    let addonUrl: string = "";
+    let fullUrl: string = this.baseUrl + addonUrl;
+
+    return this.http.post<Message>(fullUrl, form, this.httpOptions);
+  }
+
+  updateCurrentForm(form: CovidQuestion){
+    let addonUrl: string = "";
+    let fullUrl: string = this.baseUrl + addonUrl;
+
+    return this.http.put<Message>(fullUrl, form, this.httpOptions);
+  }
 }
