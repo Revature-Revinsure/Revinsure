c0433aaa87c5438dd9646f364b450e580bf85da5
lilmissrayna <lilmissrayna@gmail.com>
2021-11-08 13:44:26 -0600

    finish merging

--- RevinsureAngular/src/app/service/data.service.ts
+++ RevinsureAngular/src/app/service/data.service.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
+import { UserInfo } from '../models/user-info';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-<<<<<<< HEAD
-  currentUser!: User;
-=======
-
   public baseURL: string = "http://localhost:8000";
   userInfo!: UserInfo;
   currentUser!: User;
-
->>>>>>> 920d40b20e02eb9b4785972ee16b573c3439a4e9
   constructor() { }
 
   get BaseURL(){
