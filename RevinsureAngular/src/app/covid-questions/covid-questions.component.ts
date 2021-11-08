cc122b7a0778abbdaaa9656e07e3e9fddf4281ea
Jihad Abdul-Latif <jihadabdullatif1@gmail.com>
2021-11-08 12:56:01 -0500

    covid htmlTs

--- RevinsureAngular/src/app/covid-questions/covid-questions.component.ts
+++ RevinsureAngular/src/app/covid-questions/covid-questions.component.ts
@@ -7,9 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CovidQuestionsComponent implements OnInit {
 
+  currentDate: number = Date.now();
+  message: string | undefined = "";
+
+  //As a User, I should have to submit a form stating whether I have had, or currently have, covid-19.have uou had it or been exposed
   constructor() { }
 
   ngOnInit(): void {
   }
+  haveORnot:boolean = false;
+  haveOrbeenA:boolean =false;
+
+  submit(event:any){
+    //make a modle for covid input
+  }
+
+  //model have 
+  /*
+  export interface CovidQuestion {
+    hasCovid: boolean,
+    aroundCovid: boolean,
+    dateAnswered: Date | number;
+}
+
+
+  */
+
+onChangedB(value:boolean){
+  this.haveOrbeenA = value;
+  console.log(this.haveOrbeenA);
+}
+
+  //value:boolean
+  onChangedA(value:boolean){
+    this.haveORnot = value;
+    /*
+    let claim: Claim = {id: -1,
+      dateOfService: null,
+      dateOfClaim: this.currentDate,
+      amount: <number>(<unknown>amount),
+      status: Status.PENDING,
+      description: description};
+    */
+      console.log(this.haveORnot);
+
+    /*this.claimService.makeClaim(claim).subscribe(
+      response => {
+        this.message= response.body?.message;
+
+        console.log(this.message);
+      }
+    );*/
+
+  }
+
 
 }
